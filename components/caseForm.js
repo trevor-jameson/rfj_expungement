1e4774d19de0db4f92c4db0fbd84b639a6e3c03c
Jonah's MacbookAir <puckybreg@gmail.com>
2019-09-21 14:38:59 -0400

    Adding caseForm.js

---
+++ components/caseForm.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText
+} from "@material-ui/core";
+
+// Client name, DOB, case #, 3 others undertim
+const Home = () => (
+  <div>
+    <FormControl>
+      <InputLabel htmlFor="my-input">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" />
+      <FormHelperText id="my-helper-text">
+        We'll never share your email.
+      </FormHelperText>
+    </FormControl>
+    <FormControl>
+      <InputLabel htmlFor="my-input">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" />
+      <FormHelperText id="my-helper-text">
+        We'll never share your email.
+      </FormHelperText>
+    </FormControl>
+
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+      .card {
+        padding: 18px 18px 24px;
+        width: 220px;
+        text-align: left;
+        text-decoration: none;
+        color: #434343;
+        border: 1px solid #9b9b9b;
+      }
+      .card:hover {
+        border-color: #067df7;
+      }
+      .card h3 {
+        margin: 0;
+        color: #067df7;
+        font-size: 18px;
+      }
+      .card p {
+        margin: 0;
+        padding: 12px 0 0;
+        font-size: 13px;
+        color: #333;
+      }
+    `}</style>
+  </div>
+);
+
+export default Home;
